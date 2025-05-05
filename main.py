b1ad3ed284d9e6f32983c5c6a77d2de7fa1666b1
hamid-emam <hamideltejani@gmail.com>
2025-05-05 17:26:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from handlers.start_handler import register_start_handler
 from handlers.list_handler import register_list_handler
 from handlers.get_handler import register_get_handler
 from handlers.menu_handler import register_menu_handler
-from keep_alive import keep_alive
+
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -14,8 +14,7 @@ register_list_handler(bot)
 register_get_handler(bot)
 register_menu_handler(bot)
 
-# Start the keep-alive server (Flask)
-keep_alive()
+
 
 # Start polling for bot messages
 bot.infinity_polling()
