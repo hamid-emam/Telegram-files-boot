11a7f25c0e786f9294642a973a0883b3ffd427fd
hamid-emam <hamideltejani@gmail.com>
2025-05-06 11:00:12 +0200

    Update main.py
    Keep alive has been added again

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from handlers.list_handler import register_list_handler
 from handlers.get_handler import register_get_handler
 from handlers.menu_handler import register_menu_handler
 from handlers.add_handler import register_add_handler
-
+from keep_alive import keep_alive
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -16,6 +16,8 @@ register_get_handler(bot)
 register_menu_handler(bot)
 register_add_handler(bot)
 
+# Start the keep-alive server (Flask)
+keep_alive()
 
 # Start polling for bot messages
 bot.infinity_polling()
