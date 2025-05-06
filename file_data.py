bfc3821b6711d63c4c28966b1acedf8ca417eea7
hamid-emam <hamideltejani@gmail.com>
2025-05-06 12:49:38 +0200

    Update file_data.py
    Aded Biostatistics section

--- file_data.py
+++ file_data.py
@@ -5,5 +5,8 @@ files_by_section = {
     "Epidemiology": {
         "Epidemiology 1": "https://docs.google.com/presentation/d/1dIHqkqKf1lD47k1dHFzh1xWpyMby0PFz/export/pdf",
         "Epidemiology 2": "https://docs.google.com/presentation/d/1dBWcNt2rqpTCzv61Ri2XoLP-CkGChdDA/export/pdf"
+    },
+    "Biostatistics": { 
+        "Biostatistics 1": "https://drive.google.com/uc?export=download&id=1fvvWFFnnNufE6AO4cbZywcfuUnJG9BM7"
     }
 }
